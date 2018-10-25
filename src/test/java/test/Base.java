9d112bcc62f1fe707b7837c85d4eb69ffe76b7ad
varsinis <varsini@orzota.com>
2018-10-25 13:13:59 +0000

    Added  Filess

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -1,6 +1,5 @@
 /*
  * The MIT License
-
  * Copyright 2016 CloudBees, Inc.
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
